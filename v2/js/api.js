7e1f5efe8f04d20e7d149e845f9a9c6d0a85195a
rubenavone <navone_ruben@msn.com>
2019-02-08 14:11:46 +0100

    maj

---
+++ v2/js/api.js
@@ -0,0 +1,3 @@
+
+let url = "https://api.themoviedb.org/3/movie/542?api_key=bbad9d275376358a8c117caf83f0cba0";
+let urlMovie = "https://api.themoviedb.org/3/discover/movie?api_key=bbad9d275376358a8c117caf83f0cba0+%22&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
--- v2/js/main.js
+++ v2/js/main.js
@@ -4,47 +4,192 @@ let films = [
         realisateur: 'stanley kubrick',
         nbEntree: 13000,
         synopsis: 'la mort evidement',
-        dateSortie: '23/08/2002'
+        dateSortie: '23/08/2002',
+        acteur: ['richard dean arnderson', 'paul newman', 'sandra bullock']
     },
     {
         titre: 'Orange Mecanique',
         realisateur: 'stanley kubrick',
         nbEntree: 9000,
         synopsis: 'la mort evidement mais a plusieur',
-        dateSortie: '12/04/1992'
+        dateSortie: '12/04/1992',
+        acteur: ['Batman', 'Goldman', 'Richman']
     },
     {
         titre: 'Halloween',
         realisateur: 'john carpenter',
         nbEntree: 18000,
         synopsis: 'la mort evidement avec un masque',
-        dateSortie: '08/06/1998'
+        dateSortie: '08/06/1998',
+        acteur: ['Steve jonhson', 'Jim carrey', 'John Coltrane']
     },
     {
+        titre: 'Charlie et la chocolaterie',
+        realisateur: 'john carpenter',
+        nbEntree: 23000,
+        synopsis: 'la mort evidement avec des bonbon',
+        dateSortie: '20/03/1975',
+        acteur: ['Damas', 'Robert downey', 'paul mcartney']
+    }, {
         titre: 'Shining',
         realisateur: 'stanley kubrick',
         nbEntree: 13000,
         synopsis: 'la mort evidement',
-        dateSortie: '23/08/2002'
+        dateSortie: '23/08/2002',
+        acteur: ['richard dean arnderson', 'paul newman', 'sandra bullock']
+    },
+    {
+        titre: 'Orange Mecanique',
+        realisateur: 'stanley kubrick',
+        nbEntree: 9000,
+        synopsis: 'la mort evidement mais a plusieur',
+        dateSortie: '12/04/1992',
+        acteur: ['Batman', 'Goldman', 'Richman']
     },
     {
         titre: 'Halloween',
         realisateur: 'john carpenter',
         nbEntree: 18000,
         synopsis: 'la mort evidement avec un masque',
-        dateSortie: '08/06/1998'
+        dateSortie: '08/06/1998',
+        acteur: ['Steve jonhson', 'Jim carrey', 'John Coltrane']
     },
     {
+        titre: 'Charlie et la chocolaterie',
+        realisateur: 'john carpenter',
+        nbEntree: 23000,
+        synopsis: 'la mort evidement avec des bonbon',
+        dateSortie: '20/03/1975',
+        acteur: ['Damas', 'Robert downey', 'paul mcartney']
+    }, {
         titre: 'Shining',
         realisateur: 'stanley kubrick',
         nbEntree: 13000,
         synopsis: 'la mort evidement',
-        dateSortie: '23/08/2002'
+        dateSortie: '23/08/2002',
+        acteur: ['richard dean arnderson', 'paul newman', 'sandra bullock']
+    },
+    {
+        titre: 'Orange Mecanique',
+        realisateur: 'stanley kubrick',
+        nbEntree: 9000,
+        synopsis: 'la mort evidement mais a plusieur',
+        dateSortie: '12/04/1992',
+        acteur: ['Batman', 'Goldman', 'Richman']
+    },
+    {
+        titre: 'Halloween',
+        realisateur: 'john carpenter',
+        nbEntree: 18000,
+        synopsis: 'la mort evidement avec un masque',
+        dateSortie: '08/06/1998',
+        acteur: ['Steve jonhson', 'Jim carrey', 'John Coltrane']
+    },
+    {
+        titre: 'Charlie et la chocolaterie',
+        realisateur: 'john carpenter',
+        nbEntree: 23000,
+        synopsis: 'la mort evidement avec des bonbon',
+        dateSortie: '20/03/1975',
+        acteur: ['Damas', 'Robert downey', 'paul mcartney']
+    }, {
+        titre: 'Shining',
+        realisateur: 'stanley kubrick',
+        nbEntree: 13000,
+        synopsis: 'la mort evidement',
+        dateSortie: '23/08/2002',
+        acteur: ['richard dean arnderson', 'paul newman', 'sandra bullock']
+    },
+    {
+        titre: 'Orange Mecanique',
+        realisateur: 'stanley kubrick',
+        nbEntree: 9000,
+        synopsis: 'la mort evidement mais a plusieur',
+        dateSortie: '12/04/1992',
+        acteur: ['Batman', 'Goldman', 'Richman']
+    },
+    {
+        titre: 'Halloween',
+        realisateur: 'john carpenter',
+        nbEntree: 18000,
+        synopsis: 'la mort evidement avec un masque',
+        dateSortie: '08/06/1998',
+        acteur: ['Steve jonhson', 'Jim carrey', 'John Coltrane']
+    },
+    {
+        titre: 'Charlie et la chocolaterie',
+        realisateur: 'john carpenter',
+        nbEntree: 23000,
+        synopsis: 'la mort evidement avec des bonbon',
+        dateSortie: '20/03/1975',
+        acteur: ['Damas', 'Robert downey', 'paul mcartney']
+    }, {
+        titre: 'Shining',
+        realisateur: 'stanley kubrick',
+        nbEntree: 13000,
+        synopsis: 'la mort evidement',
+        dateSortie: '23/08/2002',
+        acteur: ['richard dean arnderson', 'paul newman', 'sandra bullock']
+    },
+    {
+        titre: 'Orange Mecanique',
+        realisateur: 'stanley kubrick',
+        nbEntree: 9000,
+        synopsis: 'la mort evidement mais a plusieur',
+        dateSortie: '12/04/1992',
+        acteur: ['Batman', 'Goldman', 'Richman']
+    },
+    {
+        titre: 'Halloween',
+        realisateur: 'john carpenter',
+        nbEntree: 18000,
+        synopsis: 'la mort evidement avec un masque',
+        dateSortie: '08/06/1998',
+        acteur: ['Steve jonhson', 'Jim carrey', 'John Coltrane']
+    },
+    {
+        titre: 'Charlie et la chocolaterie',
+        realisateur: 'john carpenter',
+        nbEntree: 23000,
+        synopsis: 'la mort evidement avec des bonbon',
+        dateSortie: '20/03/1975',
+        acteur: ['Damas', 'Robert downey', 'paul mcartney']
+    }, {
+        titre: 'Shining',
+        realisateur: 'stanley kubrick',
+        nbEntree: 13000,
+        synopsis: 'la mort evidement',
+        dateSortie: '23/08/2002',
+        acteur: ['richard dean arnderson', 'paul newman', 'sandra bullock']
+    },
+    {
+        titre: 'Orange Mecanique',
+        realisateur: 'stanley kubrick',
+        nbEntree: 9000,
+        synopsis: 'la mort evidement mais a plusieur',
+        dateSortie: '12/04/1992',
+        acteur: ['Batman', 'Goldman', 'Richman']
+    },
+    {
+        titre: 'Halloween',
+        realisateur: 'john carpenter',
+        nbEntree: 18000,
+        synopsis: 'la mort evidement avec un masque',
+        dateSortie: '08/06/1998',
+        acteur: ['Steve jonhson', 'Jim carrey', 'John Coltrane']
+    },
+    {
+        titre: 'Charlie et la chocolaterie',
+        realisateur: 'john carpenter',
+        nbEntree: 23000,
+        synopsis: 'la mort evidement avec des bonbon',
+        dateSortie: '20/03/1975',
+        acteur: ['Damas', 'Robert downey', 'paul mcartney']
     }
 ];
 
 //document.getElementById('js-film').innerHTML = '<h1> Film disponible</h1>';
-
+/* 
 films.forEach(element => {
 
     let filmSelection = document.getElementById('js-film');
@@ -52,9 +197,11 @@ films.forEach(element => {
         filmSelection.innerHTML += `<div><p> Titre du film: ${element.titre}</p>
                                 <p>Réalisateur: ${element.realisateur}</p>
                                 <p>Nombre d'entrée vendu: ${element.nbEntree}</p>                             
-                                <p>Synopsis du film: ${element.synopsis}</p>
-                                <p>Date de sortie: ${element.dateSortie}</p></div>`;
-});                                                           
+                                <p>Synopsis: ${element.synopsis}</p>
+                                <p>Date de sortie: ${element.dateSortie}</p>
+                                <button>Clique sur moi</button></div>`;
+                            
+});    */                                                        
 
 /* //for each avec l'ecriture ecmaScript
 films.forEach(element => {
@@ -70,13 +217,61 @@ films.forEach(element => {
     filmSelection.insertAdjacentHTML("beforeend" ,filmAffichage); 
                                 
 }); */
+/* 
+let btn = document.getElementById
 
-let title = document.querySelector("body");
+document.onclick = changeBackground;
+
+changeBackground(e) => {
+
+} */
+
 
 //document.querySelector("header").insertAdjacentHTML("afterbegin", "<h1> Liste de film </h1>"); 
 
 
-document.querySelector("footer").insertAdjacentHTML("beforeend" , '<footer> Copyright la france </footer>');
+//document.querySelector("footer").insertAdjacentHTML("beforeend" , '<footer> Copyright la france </footer>');
+
+//let cssModif = document.createElement('H2').textContent="bisous";
+
+//document.getElementById('js-film').appendChild(cssModif).style.textAlign("center");
+
+
+
+
+
+   var xhr = new XMLHttpRequest();
+   xhr.open('GET', 'https://api.themoviedb.org/3/discover/movie?api_key=bbad9d275376358a8c117caf83f0cba0&language=fr-FR&sort_by=with_genres=18%26primary_release_year=2014&include_adult=true&include_video=true&page=2');
+   xhr.onload = function() {
+       if (xhr.status === 200) {
+        var e = JSON.parse(xhr.responseText);
+        console.log(e.results[0].title)
+        e.results.forEach(element => {
+
+            let filmSelection = document.getElementById('js-film');
+               
+                filmSelection.innerHTML += `<article><h2>${element.title}</h2>
+                                        <p>Nombre d'entrée vendu: ${element.nbEntree}</p>                             
+                                        <p>Date de sortie: ${element.release_date}</p>
+                                        <figure>
+                                        <img src="https://image.tmdb.org/t/p/w200${element.poster_path}" alt="image du film: ${element.title}"> 
+                                        <figcaption>Synopsis: ${element.overview} </figcaption>
+                                        </figure>
+                                        </article>`;
+                                    
+        });
+       }
+       else {
+           alert(`La requete a échouer, code de l'erreur: ${xhr.status}`);
+       }
+   };
+   xhr.send();
+
+
+
+
+
+
 
 //Deux façon d'écrire//
 console.log("hello world " + films[0].titre);
